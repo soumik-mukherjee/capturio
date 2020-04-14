d1160d1159d93890dba1f6562a413877f10f1bfa
soumik-mukherjee <me@soumikmukherjee.dev>
2020-04-14 21:06:10 +0530

    Tested with new scenario stories

--- packages/ui-components/src/Stories/index.stories.js
+++ packages/ui-components/src/Stories/index.stories.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button } from '@storybook/react/demo';
 
-export default { title: 'Button' };
+export default { title: 'SB Demo' };
 
 export const withText = () => <Button>Hello Button</Button>;
 
