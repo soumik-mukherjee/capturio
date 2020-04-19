d5af30b0e4ab76da9921d701ff9edeafad213202
soumik-mukherjee <60540758+soumik-mukherjee@users.noreply.github.com>
2020-04-19 14:18:23 +0530

    Changes to title and description

--- packages/webapp/gatsby-config.js
+++ packages/webapp/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Capturio - The Web recording app`,
+    description: `Record screencasts with a voiceover, all from just your browser!`,
+    author: `@soumik-mukherjee`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
